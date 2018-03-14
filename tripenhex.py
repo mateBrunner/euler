31851546f203b6ac1d142fbb3572ad88d7f2ebe3
Fábián János <fabian.janos93@gmail.com>
2018-03-14 10:21:20 +0100

    number generate complete

--- tripenhex.py
+++ tripenhex.py
@@ -2,18 +2,21 @@
 # triangular, pentagonal and hexagonal #45
 import math
 
-
+"""
 def ispentagonal(szam):
     tort = (math.sqrt(1+24*szam)+1)/6
     return tort == int(tort)
+
 
 def istriangular(szam):
     tort = (math.sqrt(1+8*szam)-1)/2
     return tort == int(tort)
 
+
 def ishexagonal(szam):
     tort = (math.sqrt(1+8*szam)+1)/4
     return tort == int(tort)
+
 
 talalat = 0
 i = 0
@@ -22,4 +25,34 @@ while talalat < 4:
     szam = i*(2*i-1)
     if ispentagonal(szam) and istriangular(szam):
         talalat += 1
-        print(szam)
+        print(szam) """
+
+
+def generating_function():
+    p3 = [1]
+    p4 = [1]
+    p5 = [1]
+    p6 = [1]
+    p7 = [1]
+    p8 = [1]
+    n = 2
+    while len(str(p3[-1])) < 5:
+        if len(str(n * (n + 1) / 2)) > 5:
+            p3.append(int(n * (n + 1) / 2))
+        if len(str(n ** 2)) > 5 and len(str(n ** 2)) < 7:
+            p4.append(int(n ** 2))
+        if len(str(n * (3 * n - 1) / 2)) > 5 and len(str(n * (3 * n - 1) / 2)) < 7:
+            p5.append(int(n * (3 * n - 1) / 2))
+        if len(str(n * (2 * n - 1))) > 5 and len(str(n * (2 * n - 1))) < 7:
+            p6.append(int(n * (2 * n - 1)))
+        if len(str(n * (5 * n - 3) / 2)) > 5 and len(str(n * (5 * n - 3) / 2)) < 7:
+            p7.append(int(n * (5 * n - 3) / 2))
+        if len(str(n * (3 * n - 2))) > 5 and len(str(n * (3 * n - 2))) < 7:
+            p8.append(int(n * (3 * n - 2)))
+        n += 1
+    p3 = p3[1:-1]
+    return p3, p4, p5, p6, p7, p8
+
+
+p3, p4, p5, p6, p7, p8 = generating_function()
+print(p3)
