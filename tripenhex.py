8614e0acbda866b8fa893e26d99e06c4a8914878
Mate Brunner <brunnerm7@gmail.com>
2018-03-14 10:23:09 +0100

    added function

--- tripenhex.py
+++ tripenhex.py
@@ -1,25 +1,67 @@
 #!/usr/bin/python3.5
-# triangular, pentagonal and hexagonal #45
+# cyclical figurate numbers #61
 import math
 
 
-def ispentagonal(szam):
-    tort = (math.sqrt(1+24*szam)+1)/6
-    return tort == int(tort)
 
-def istriangular(szam):
-    tort = (math.sqrt(1+8*szam)-1)/2
-    return tort == int(tort)
 
-def ishexagonal(szam):
-    tort = (math.sqrt(1+8*szam)+1)/4
-    return tort == int(tort)
 
-talalat = 0
-i = 0
-while talalat < 4:
-    i += 1
-    szam = i*(2*i-1)
-    if ispentagonal(szam) and istriangular(szam):
-        talalat += 1
-        print(szam)
+listak = [p3,p4,p5,p6,p7,p8]
+kilove = [0]
+
+def ciklusos(szam1,szam2):
+    if str(szam1[3:]) == str(szam2[:2]):
+        return True
+    return False
+
+for i3 in listak[0]:
+    for n1 in range(1,6):
+        if n1 not in kilove:
+            for i4 in listak[n1]:
+                if ciklusos(i3,i4):
+                    kilove.append(n1)
+                    for n2 in range(1,6):
+                        if n2 not in kilove:
+                            for i5 in listak[n2]:
+                                if ciklusos(i4,i5):
+                                    kilove.append(n2)
+                        if n2 in kilove: kilove.remove(n1)
+        if n1 in kilove: kilove.remove(n1)
+                    
+"""
+for i3 in listak[0]:
+    for n1 in range(1,6):
+        if n1 not in kilove:
+            for i4 in listak[n1]:
+                if ciklusos(i3,i4):
+                    kilove.append(n1)
+                    for n2 in range(1,6):
+                        if n2 not in kilove:
+                            for i5 in listak[n2]:
+                                if ciklusos(i4,i5):
+                                    kilove.append(n2)
+
+                                    for n2 in range(1,6):
+                                        if n2 not in kilove:
+                                            for i5 in listak[n2]:
+                                                if ciklusos(i4,i5):
+                                                    kilove.append(n2)
+
+                                                    for n2 in range(1,6):
+                                                        if n2 not in kilove:
+                                                            for i5 in listak[n2]:
+                                                                if ciklusos(i4,i5):
+                                                                    kilove.append(n2)
+
+                                                                    for n2 in range(1,6):
+                                                                        if n2 not in kilove:
+                                                                            for i5 in listak[n2]:
+                                                                                if ciklusos(i4,i5):
+                                                                                    kilove.append(n2)
+
+
+
+                        if n2 in kilove: kilove.remove(n1)
+        if n1 in kilove: kilove.remove(n1)
+
+"""
